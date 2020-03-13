d70c42168f1db36a26ea268876b0609b4d894fa5
ianrios <ian.rios1a@gmail.com>
2020-03-13 12:18:12 -0400

    added d3 graph file

--- src/components/pages/Atlas.js
+++ src/components/pages/Atlas.js
@@ -1,11 +1,95 @@
 import React from 'react'
+import { Graph } from "react-d3-graph";
+import data from "../../constants/d3.js"
+
 
 export default function Atlas() {
+
+
+	const myConfig = {
+		nodeHighlightBehavior: true,
+		node: {
+			color: "#68a7ff",
+			fontColor: "white",
+			size: 120,
+			highlightStrokeColor: "blue",
+		},
+		link: {
+			color: "white",
+			highlightColor: "lightblue",
+		},
+	};
+
+	const onClickGraph = function () {
+		console.log(`Clicked the graph background`);
+	};
+
+	const onClickNode = function (nodeId) {
+		console.log(`Clicked node ${nodeId}`);
+	};
+
+	const onDoubleClickNode = function (nodeId) {
+		console.log(`Double clicked node ${nodeId}`);
+	};
+
+	const onRightClickNode = function (event, nodeId) {
+		console.log(`Right clicked node ${nodeId}`);
+	};
+
+	const onMouseOverNode = function (nodeId) {
+		console.log(`Mouse over node ${nodeId}`);
+	};
+
+	const onMouseOutNode = function (nodeId) {
+		console.log(`Mouse out node ${nodeId}`);
+	};
+
+	const onClickLink = function (source, target) {
+		console.log(`Clicked link between ${source} and ${target}`);
+	};
+
+	const onRightClickLink = function (event, source, target) {
+		console.log(`Right clicked link between ${source} and ${target}`);
+	};
+
+	const onMouseOverLink = function (source, target) {
+		console.log(`Mouse over in link between ${source} and ${target}`);
+	};
+
+	const onMouseOutLink = function (source, target) {
+		console.log(`Mouse out link between ${source} and ${target}`);
+	};
+
+	const onNodePositionChange = function (nodeId, x, y) {
+		console.log(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
+	};
 	return (
-		<div className="row main-header">
-			<div className="col">
-				<h1 className="header-sub-page">Atlas</h1>
+		<React.Fragment>
+			<div className="row main-header">
+				<div className="col">
+					<h1 className="header-sub-page">Atlas</h1>
+				</div>
 			</div>
-		</div>
+			<div className="row">
+				<div className="col">
+					<Graph
+						id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+						data={data}
+						config={myConfig}
+						onClickNode={onClickNode}
+						onDoubleClickNode={onDoubleClickNode}
+						onRightClickNode={onRightClickNode}
+						onClickGraph={onClickGraph}
+						onClickLink={onClickLink}
+						onRightClickLink={onRightClickLink}
+						onMouseOverNode={onMouseOverNode}
+						onMouseOutNode={onMouseOutNode}
+						onMouseOverLink={onMouseOverLink}
+						onMouseOutLink={onMouseOutLink}
+						onNodePositionChange={onNodePositionChange}
+					/>
+				</div>
+			</div>
+		</React.Fragment>
 	)
 }
--- src/components/pages/Reload.js
+++ src/components/pages/Reload.js
@@ -4,14 +4,18 @@ import { Link } from "react-router-dom";
 export default function Reload() {
 
 	return (
-		<div>
-			<h1 className="header-sub-page">Click The Button</h1>
-			<button type="button" class="btn btn-secondary mb-3" onClick={() => {
-				window.location.reload(true);
-			}}>Hard Reload Button</button>
-			<h2>
-				<Link to={"/"}>Back Home</Link>
-			</h2>
+		<div className="row center-contact ">
+			<div className="col-10">
+				<h1 className="header-sub-page">Click The Button</h1>
+				<p className="questrial">If this button does not fix the cache clearing bug with the service worker, try clearing your application data, doing a hard reload.</p>
+				<p className="questrial">(<code>cmd+shift+r</code> on mac or <code>ctrl+shift+r</code> on windows)</p>
+				<button type="button" class="btn btn-secondary mb-3" onClick={() => {
+					window.location.reload(true);
+				}}>Hard Reload Button</button>
+				<h2>
+					<Link to={"/"}>Back Home</Link>
+				</h2>
+			</div>
 		</div>
 	)
 }
