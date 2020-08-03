58171ed35bbdcb52f23a5abe3376ff8936c4d6e5
ianrios <ian.rios1a@gmail.com>
2020-08-03 15:45:14 -0400

    added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,24 +4,30 @@ import { Link } from "react-router-dom";
 
 export default function Contact() {
 	return (
-		<div className="row center-contact ">
+		<div className="row center-contact">
 			<div className="col-10">
 				<h1 className="header-sub-page">Contact</h1>
-				<p >
-					If the website does not load a new page that should exist, first <Link to="/hard-reload">try this button</Link>, try closing all tabs, a <a target="_blank"
-						rel="noopener noreferrer" href="https://refreshyourcache.com/en/cache/">hard reload</a>, and even opening a new incognito tab. If none of that works, shoot us a message.
-				</p>
-				<p>You can send us an email at</p>
-				<p>
-					<a href={`mailto:whyrecordcompany@gmail.com`}>whyrecordcompany@gmail.com</a>
-				</p>
-				<p>or visit us on social media</p>
-				<p>
-					<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/whyrecord">twitter</a>{" - "}
-					<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/whyrecordcompany">instagram</a>{" - "}
-					<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/WHYRecordCompany">facebook</a>
-				</p>
-				<p>If you would like to release on WRC, please fill out <a href="https://forms.gle/pDcYS2Kmd5rvDJVw8" target="_blank" rel="noopener noreferrer">this form</a> to submit a demo. We will get back to you within a week.</p>
+				<div className="questrial">
+					<p >
+						If the website does not load a page that should exist,
+						first <Link to="/hard-reload">try this button</Link>,
+						next, try closing all tabs,
+						then, try a <a target="_blank" rel="noopener noreferrer" href="https://refreshyourcache.com/en/cache/">hard reload</a>,
+						and finally, opening a new incognito tab.
+						If none of that works, shoot us a message.
+					</p>
+					<p>You can send us an email at</p>
+					<p>
+						<a href={`mailto:admin@whyrecord.com`}>admin@whyrecord.com</a>
+					</p>
+					<p>or visit us on social media</p>
+					<p>
+						<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/whyrecord">twitter</a>{" - "}
+						<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/whyrecordcompany">instagram</a>{" - "}
+						<a className="no-style-link" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/WHYRecordCompany">facebook</a>
+					</p>
+					<p>If you would like to release on WRC, please fill out <a href="https://forms.gle/pDcYS2Kmd5rvDJVw8" target="_blank" rel="noopener noreferrer">this form</a> to submit a demo. We will get back to you within a week.</p>
+				</div>
 			</div>
 		</div>
 	)
