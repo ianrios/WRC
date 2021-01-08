3ef63ae3c448647c8492a06b693fb1f7c55905c7
ianrios <ian.rios1a@gmail.com>
2021-01-08 17:15:29 -0500

    fixed live page

--- src/components/pages/LivePage.js
+++ src/components/pages/LivePage.js
@@ -5,13 +5,14 @@ import { TwitchEmbed } from 'react-twitch-embed';
 function LivePage() {
     return (
         <div className="row center-contact">
-            <div className="col-10">
+            <div className="col-12">
                 <h1 className="header-sub-page">Livestreams</h1>
                 <div className="questrial">
                     <TwitchEmbed
                         channel="lom564"
                         id="lom564"
                         theme="dark"
+                        width="100%"
                         onVideoPause={() => console.log(':(')}
                     />
                 </div>
