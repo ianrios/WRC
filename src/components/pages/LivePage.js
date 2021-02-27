aab84998d10f3d272e419d1e59c103a3ee2e970e
ianrios <ian.rios1a@gmail.com>
2021-02-27 14:13:42 -0500

    added bl09

--- src/components/pages/LivePage.js
+++ src/components/pages/LivePage.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { TwitchEmbed } from 'react-twitch-embed';
 
 
 function LivePage() {
-    const twitchUsers = ["lom564", 'of00t', "quantopix", 'euphorixeventsofficial']
+    const twitchUsers = ["lom564", "lom564", "quantopix", "lom564", "lom564", "lom564", "of00t"]
+    const [weekday, setWeekday] = useState(new Date().getDay())
+    useEffect(() => setInterval(() => setWeekday(new Date().getDay(), 1000)), [])
     // TODO: add this to sidebar
+
+    const currentTwitchUser = useMemo(() => twitchUsers[weekday], [weekday])
     return (
         <div className="row center-contact">
             <div className="col-12">
                 <h1 className="header-sub-page">Livestreams</h1>
                 <div className="questrial">
                     <TwitchEmbed
-                        channel={twitchUsers[0]}
-                        id={twitchUsers[0]}
+                        channel={currentTwitchUser}
+                        id={currentTwitchUser}
                         theme="dark"
                         width="100%"
                         onVideoPause={() => console.log(':(')}
