88e85337cb543526026c89cf061e41cb3a383936
Ian Rios <ian.rios1a@gmail.com>
2023-11-16 14:46:56 -0500

    added new info for why100 release

--- src/components/pages/ReleaseGrid.js
+++ src/components/pages/ReleaseGrid.js
@@ -3,8 +3,30 @@ import "./ProductPage.scss";
 
 export default function ReleaseGrid() {
 
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex > 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+
+
     const length = '114px'
     let index = 0
+
+    const shuffledReleases = shuffle(releaseData)
     return (
         <>
             <div className="row main-header">
@@ -25,14 +47,16 @@ export default function ReleaseGrid() {
                 return (
                     <div className="row" key={idx}>
                         {[...Array(10).keys()].map((_j, jdx) => {
-                            const r = releaseData[index]
+                            const r = shuffledReleases[index]
                             // WHY75-A was made to fix the lack of WHY064 - my mistake when originally releasing
                             // WHY064 was only released on soundcloud (as of 02/12/23)
                             index += 1
                             return (
                                 <div className="col" width={length} key={jdx}>
-                                    {r &&
-                                        <img height={length} width={length} alt={r.name} src={r.album_art} />
+                                    {r ?
+                                        <img height={length} width={length} alt={r.name} src={r.album_art} /> :
+                                        <img height={length} width={length} alt={shuffledReleases[15].name} src={shuffledReleases[15].album_art} />
+
                                     }
                                 </div>
                             )
